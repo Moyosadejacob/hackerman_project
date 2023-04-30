0b846cfdb7596f9bc3ca56ee7c38ecd218845da3
Moyosade Jacob <108759457+Moyosadejacob@users.noreply.github.com>
2023-04-30 15:06:51 +0300

    remodelling

--- app.py
+++ app.py
@@ -7,28 +7,26 @@ from autogluon.tabular import TabularPredictor
 
 st.title('sentiment Prediction')
 friends = st.number_input('author.properties.friends', 0,2000)
-status_count = st.number_input('author.properties.status_count', 0,2000)
+status_count = st.number_input('author.properties.status_count', 0,50000)
 verified = st.selectbox('author.properties.verified', options=['True', 'False'])
-body = st.selectbox('content.body', options=["Can't believe I'm missing Love Island 😩", 'How many times does he wonna say the phrase "i deal with shit" #LoveIsland', "@Amyyy14 thank u so much Amy you really get me ❤️ I come home tmrw let's get drinks"])
+body = st.selectbox('content.body', options=["Can't believe I'm missing Love Island 😩", 'How many times does he wonna say the phrase "i deal with shit" #LoveIsland', "@Amyyy14 thank u so much Amy you really get me ❤️ I come home tmrw let's get drinks", "@sickkening Yep you're also that xx"])
 country = st.selectbox('location.country', options=['GB', 'GG', 'IM', 'JE'])
 platform = st.selectbox('properties.platform', options=['twitter'])
-sentiment = st.selectbox("properties.sentiment", options=[ 1., -1., 0.])
 latitude = st.number_input('location.latitude', 0,100)
 longitude = st.number_input('location.longitude', -5,5)
-sentiments=st.form('Sentiments prediction', clear_on_submit=False)
+sentiments=st.form('Sentiments prediction', clear_on_submit=True)
 
 
 input_data_dict = {
     
-    "author.properties.friends": 114,
-    "author.properties.status_count": 1377,
-    "author.properties.verified": "True",
-    "content.body": "Can't believe I'm missing Love Island 😩",
-    "location.country": 'GG',
-    "properties.platform": 'twitter',
-    "properties.sentiment": 1,
-    "location.latitude": 52.96974444,
-    "location.longitude": -1.172266
+    "author.properties.friends": friends,
+    "author.properties.status_count": status_count,
+    "author.properties.verified": verified,
+    "content.body": body,
+    "location.country": country,
+    "properties.platform": platform,
+    "location.latitude": latitude,
+    "location.longitude": longitude
     
 }
 st.write(input_data_dict)
